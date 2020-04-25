f2e8fe31e3477624634819a9cae39c383d861772
thoriqdharmawan <thoriqd21@gmail.com>
2020-04-25 10:29:00 +0700

    Updates

---
+++ precache-manifest.71871acb75b8572c29c8a75244970469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b068e7e5a62f61c71ba2f580eca01e9b",
+    "url": "/template/index.html"
+  },
+  {
+    "revision": "9223f75d69098b88b73a",
+    "url": "/template/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f2236599c2be6d5484c8",
+    "url": "/template/static/js/2.d036dfd9.chunk.js"
+  },
+  {
+    "revision": "6e227778e79b9500741a92d00261c69d",
+    "url": "/template/static/js/2.d036dfd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9223f75d69098b88b73a",
+    "url": "/template/static/js/main.6346dd34.chunk.js"
+  },
+  {
+    "revision": "eb71c825cf8c754fa4f9",
+    "url": "/template/static/js/runtime-main.cb0e243c.js"
+  }
+]);
